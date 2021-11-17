55a4cc181ccff52ca84d19c37346b5962b29c2de
jacksloan <jbsloan10@gmail.com>
2021-11-17 14:23:25 -0600

    remove airtkit app

--- packages/airtable-codegen/src/lib/airtable-codegen.ts
+++ packages/airtable-codegen/src/lib/airtable-codegen.ts
@@ -1,82 +1,4 @@
-import 'reflect-metadata';
-
-import * as _ from 'lodash';
 import airtable from 'airtable';
-
-const allowedPropTypes = ['String', 'Boolean', 'Number'] as const;
-type allowedPropType = typeof allowedPropTypes[number];
-
-function airProp(target: any, key: string): void {
-  const t = Reflect.getMetadata('design:type', target, key);
-  registerProperty(target, key);
-  const type: allowedPropType | string = t.name;
-  if (!allowedPropTypes.includes(type as any)) {
-    throw new Error(
-      `Invalid airtable property ${key}. Only types \`string\`, \`number\`, or \`boolean\` are valid`
-    );
-  }
-  if (delete target[key]) {
-    const prop = `_${key}`;
-    Object.defineProperty(target, key, {
-      get: () => target[prop],
-      set: (value: unknown) => {
-        switch (type) {
-          case 'Boolean':
-            if (_.isBoolean(value)) {
-              target[prop] = value;
-            }
-            break;
-          case 'Number':
-            if (_.isNumber(value)) {
-              target[prop] = value;
-            }
-            break;
-          case 'String':
-            if (_.isString(value)) {
-              target[prop] = value;
-            }
-            break;
-        }
-      },
-      enumerable: false,
-      configurable: true,
-    });
-  }
-}
-
-const fieldsMetadataKey = Symbol('airtableFields');
-
-function registerProperty(
-  target: Record<string, unknown>,
-  propertyKey: string
-): void {
-  let properties: string[] = Reflect.getMetadata(fieldsMetadataKey, target);
-
-  if (properties) {
-    properties.push(propertyKey);
-  } else {
-    properties = [propertyKey];
-    Reflect.defineMetadata(fieldsMetadataKey, properties, target);
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-function AirtableEntity(constructor: Function) {
-  constructor.prototype.fromRecord = function (record: {
-    get: (property: string) => string | boolean | number;
-  }) {
-    const fieldNames: string[] = Reflect.getMetadata(fieldsMetadataKey, this);
-    fieldNames.forEach((field) => (this[field] = record.get(field)));
-  };
-}
-
-@AirtableEntity
-class TestClass {
-  @airProp stringProp?: string;
-  @airProp boolProp?: boolean;
-  @airProp numberProp?: number;
-  // @airProp arrayProp?: any[];
-}
 
 type booleanType = 'boolean';
 type stringType = 'string';
@@ -89,35 +11,33 @@ type lookupType<T> = T extends booleanType
   ? number
   : T;
 
-type Api<spec> = {
-  [tableKey in keyof spec]: {
+export type AirtableEntity<Model> = {
+  [key in keyof Model]: lookupType<Model[key]>;
+};
+
+type AirtableApi<Spec> = {
+  [key in keyof Spec]: {
     findAll(options?: {
-      sort?: Array<{ field: keyof spec[tableKey]; direction: 'asc' | 'desc' }>;
-    }): Promise<
-      Array<{
-        [fieldKey in keyof spec[tableKey]]: lookupType<
-          spec[tableKey][fieldKey]
-        >;
-      }>
-    >;
+      sort?: Array<{ field: keyof Spec[key]; direction: 'asc' | 'desc' }>;
+    }): Promise<Array<AirtableEntity<Spec[key]>>>;
   };
 };
 
-function createApi<S>(options: {
+export function createApi<S>(options: {
   spec: S;
   apiKey: string;
   baseId: string;
-}): Api<S> {
+}): AirtableApi<S> {
   const { spec, apiKey, baseId } = options;
   const base = new airtable({ apiKey }).base(baseId);
 
   const handler = {
     get: function (target: any, prop: string, receiver: any) {
       return {
-        async findAll(options?: { sort: any }) {
+        async findAll(options?: { sort?: any }) {
           const records = await base
             .table(prop as string)
-            .select()
+            .select(options)
             .all();
           return records.map((r) => {
             return Object.keys((spec as any)[prop]).reduce((acc, curr) => {
@@ -132,38 +52,3 @@ function createApi<S>(options: {
 
   return new Proxy({}, handler);
 }
-
-const api = createApi({
-  apiKey: 'TODO',
-  baseId: 'TODO',
-  spec: {
-    places: {
-      name: 'string',
-      lat: 'number',
-      long: 'number',
-      active: 'boolean',
-    },
-    locations: {
-      name: 'string',
-      description: 'string',
-      latitude: 'number',
-      longitude: 'number',
-    },
-    people: {
-      firstName: 'string',
-      lastName: 'string',
-      age: 'number',
-    },
-  } as const,
-});
-
-(async () => {
-  const places = await api.places.findAll();
-  const locations = await api.locations.findAll();
-  const people = await api.people.findAll();
-  console.log({
-    places,
-    locations,
-    people,
-  });
-})();
