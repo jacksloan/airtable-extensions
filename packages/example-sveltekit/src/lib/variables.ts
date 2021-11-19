7a929e7a0255097bf0f028334dd418f41d719dab
jacksloan <jbsloan10@gmail.com>
2021-11-19 13:58:26 -0600

    remove sveltekit example app

--- packages/example-sveltekit/src/lib/variables.ts
+++
@@ -1,4 +0,0 @@
-export const variables = {
-    apiKey: process.env.AIRTABLE_KEY,
-	baseId: process.env.AIRTABLE_BASE
-}
--- packages/example-sveltekit/src/routes/index.json.ts
+++
@@ -1,6 +0,0 @@
-import { api, Location } from '$lib/api';
-
-export async function get(): Promise<{ body: Location[] }> {
-	const body = await api.locations.findAll();
-	return { body };
-}
