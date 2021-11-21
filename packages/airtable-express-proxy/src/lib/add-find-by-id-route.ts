3a6d5688be1cbfbfdc146cc9ad5ca2153f530ae6
jacksloan <jbsloan10@gmail.com>
2021-11-21 15:33:31 -0600

    spelling

--- packages/airtable-express-proxy/src/lib/add-find-by-id-route.ts
+++ packages/airtable-express-proxy/src/lib/add-find-by-id-route.ts
@@ -4,10 +4,10 @@ import { AirtableApi } from 'jbs-airtable-api-extensions';
 export function addFindByIdRoute(
   expressApp: Express,
   airtable: AirtableApi<any>,
-  baseBase: string,
+  basePath: string,
   tableName: string
 ) {
-  expressApp.get(`${baseBase}/:recordId`, (req, res) => {
+  expressApp.get(`${basePath}/:recordId`, (req, res) => {
     const { recordId } = req.params;
     airtable[tableName].findById(recordId).then((value) => res.send(value));
   });
